012c1ff2af089676150bf86b90411007d9d84e2b
lexmark <lexmark@DESKTOP-TC0H06M>
2023-04-22 19:16:45 -0500

    Agregar archivos de proyecto.

---
+++ acorreaS2/Operaciones.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace acorreaS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            DisplayAlert("Mensaje de Bienvenida", "Bienvenido a la Universidad Israel " + nombre, "Cerrar ");
+
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            
+            double Deber1 = Convert.ToDouble(txtDeber1.Text)*0.30;
+            double Examen1 = Convert.ToDouble(txtExamen1.Text)*0.20;
+            double Deber2 = Convert.ToDouble(txtDeber2.Text)*0.30;
+            double Examen2 = Convert.ToDouble(txtExamen2.Text)*0.20;
+            double suma = Deber1 + Examen1;
+            txtNotaParcial1.Text = suma.ToString();
+            double Suma = Deber2 + Examen2;
+            txtNotaParcial2.Text = suma.ToString();
+            double sumar = Deber1 + Deber2 + Examen1 + Examen2;
+            txtNotaFinal.Text = sumar.ToString();
+            
+            
+        }
+
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            txtDeber1.Text = "";
+            txtExamen1.Text = "";
+            txtDeber2.Text = "";
+            txtExamen2.Text = "";
+            txtNotaFinal.Text = "";
+            txtNotaParcial1.Text = "";
+            txtNotaParcial2.Text = "";
+            txtMensaje.Text = "";
+
+        }
+
+        private void btnEvaluar_Clicked(object sender, EventArgs e)
+        {
+            double notaFinal = Double.Parse(txtNotaFinal.Text);
+            string nombre = txtMensaje.Text;
+
+            if (notaFinal >= 7)
+            {
+                nombre = "APROBADO";
+            }
+            else if (notaFinal >= 5 && notaFinal <= 6.9)
+            {
+                nombre = "Complementario";
+            }
+            else
+            {
+                nombre = "REPROBADO";
+            }
+
+            txtMensaje.Text = nombre;
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string Deber1 = txtDeber1.Text;
+            string Examen1 = txtExamen1.Text;
+            string Deber2 = txtDeber2.Text;
+            string Examen2 = txtExamen2.Text;
+            string NotaParcial1 = txtNotaParcial1.Text;
+            string NotaParcial2 = txtNotaParcial2.Text;
+            string Notafinal = txtNotaFinal.Text;
+            string Mensaje = txtMensaje.Text;
+            Navigation.PushAsync(new Registro(Deber1, Examen1, Deber2, Examen2, NotaParcial1, NotaParcial2, Notafinal, Mensaje ));
+        }
+    }
+}
---
+++ acorreaS2/Registro.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace acorreaS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string Deber1, string Examen1, string Deber2, string Examen2, string NotaParcial1, string NotaParcial2, string NotaFinal, string Mensaje)
+        {
+            InitializeComponent();
+            lblDeber1.Text = Deber1;
+            lblExamen1.Text = Examen1;
+            lblDeber2.Text = Deber2;
+            lblExamen2.Text = Examen2;
+            lblNotaParcial1.Text = NotaParcial1;
+            lblNotaparcial2.Text = NotaParcial2;
+            lblNotaFinal.Text = NotaFinal;
+            lblMensaje.Text = Mensaje;
+            //txtDeber1.Text = Deber1;
+            //txtExamen1.Text = Examen1;
+            //txtDeber2.Text = Deber2;
+            //txtExamen2.Text= Examen2;
+            //txtNotaParcial1.Text = NotaParcial1;
+            //txtNotaParcial2.Text = NotaParcial2;
+            //txtNotaFinal.Text = NotaFinal;
+            //txtMensaje.Text = Mensaje;
+        }
+    }
+}
